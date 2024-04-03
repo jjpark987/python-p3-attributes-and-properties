1b1bea3a0dae36a63146698b59befa71747c7a62
Jason Park <jjpark987@gmail.com>
2024-04-03 11:16:59 -0700

    solution

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,19 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Teddy', breed='Pointer') -> None:
+        self.set_name(name)
+        self.set_breed(breed)
+
+    def set_name(self, name):
+        if type(name) == str and len(name) > 1 and len(name) < 25:
+            self.name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print('Breed must be in list of approved breeds.')
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='Carlos', job='Admin') -> None:
+        self.set_name(name)
+        self.set_job(job)
+
+    def set_name(self, name):
+        if type(name) == str and len(name) > 1 and len(name) < 25:
+            split_name = name.split()
+            self.name = ' '.join([f'{part[0].upper()}' + f'{part[1:]}' for part in split_name])
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print('Job must be in list of approved jobs.')
